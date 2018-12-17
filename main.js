f1cade95dcf7cba4ee40844ec9c7eb2eada0482a
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-18 09:39:14 +1100

    Update Game Area

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
+var person;
+
 function startGame() {
   myGameArea.start();
+  person = new gameObject(30, 30, "green", 100, 100);
 }
 
-var myGameArea = {
+var gameArea = {
   canvas : document.createElement("canvas"),
   start : function() {
     this.canvas.width = 480;
     this.canvas.height = 270;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateGame, 20);
+  },
+  clear : function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
+}
+
+function gameObject(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y; 
+  this.update = function(){
+    ctx = gameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+function updateGame() {
+  gameArea.clear();
+  person.x += 1;
+  person.update();
 }
