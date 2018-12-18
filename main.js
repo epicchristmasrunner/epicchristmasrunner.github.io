df4bc292dfd827f543afe5c15ebb1414fbdc1db2
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-18 13:14:13 +1100

    Update main.js

--- main.js
+++ main.js
@@ -13,10 +13,13 @@ var gameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
-        window.addEventListner('', {
-            
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
         })
-    },
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
+        },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
@@ -26,15 +29,26 @@ function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
     this.x = x;
-    this.y = y;    
+    this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
     this.update = function(){
         ctx = gameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY; 
+    } 
 }
 
 function updateGameArea() {
     gameArea.clear();
+    if (person.key && person.key == 37) {person.speedX = -1; }
+    if (person.key && person.key == 39) {person.speedX = 1; }
+    if (person.key && person.key == 38) {person.speedY = -1; }
+    if (person.key && person.key == 40) {person.speedY = 1; }
+    person.newPos();
     person.update();
 }
