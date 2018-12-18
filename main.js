d460e61e5d415710f048eab584e4126cfc5634ed
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-18 13:02:33 +1100

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var person;
 
 function startGame() {
     person = new component(30, 30, "red", 10, 120);
-    myGameArea.start();
+    gameArea.start();
 }
 
-var myGameArea = {
+var gameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
         this.canvas.width = window.innerWidth - 10;
@@ -13,6 +13,9 @@ var myGameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
+        window.addEventListner('', {
+            
+        })
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -25,13 +28,13 @@ function component(width, height, color, x, y) {
     this.x = x;
     this.y = y;    
     this.update = function(){
-        ctx = myGameArea.context;
+        ctx = gameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
 function updateGameArea() {
-    myGameArea.clear();
+    gameArea.clear();
     person.update();
 }
