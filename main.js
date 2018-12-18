3f0fc6d5f0b85a77d1599306eedd4feb26b5fdcc
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-18 12:33:58 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 var person;	
- 	
- function startGame() {	
-     gameArea.start();	
-     person = new component(30, 30, "red", 10, 120);	
- }	
- 	
- var gameArea = {	
-     canvas : document.createElement("canvas"),	
-     start : function() {	
-         this.canvas.width = 480;	
-         this.canvas.height = 270;	
-         this.context = this.canvas.getContext("2d");	
-         document.body.insertBefore(this.canvas, document.body.childNodes[0]);	
-     },
+
+function startGame() {	
+    gameArea.start();	
+    person = new component(30, 30, "red", 10, 120);	
+}
+var gameArea = {	
+   canvas : document.createElement("canvas"),	
+   start : function() {	
+        this.canvas.width = window.innerWidth-10;	
+        this.canvas.height = window.innerHeight-20;	
+        this.context = this.canvas.getContext("2d");	
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);	
+    },
     clear : function() {
-         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
  }	
  	
